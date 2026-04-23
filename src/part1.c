7f43b5424416df287a49cf3eda654117f0306302
Kate Cheng <50066308+katecheng06@users.noreply.github.com>
2026-04-24 03:17:12 +0800

    Clean up comments and documentation in part1.c

--- src/part1.c
+++ src/part1.c
@@ -1,44 +1,4 @@
-/**
- * ENGG1110 Problem Solving by Programming
- *
- * Course Project - Maze
- *
- * I declare that the project here submitted is original
- * except for source material explicitly acknowledged,
- * and that the same or closely related material has not been
- * previously submitted for another course.
- * I also acknowledge that I am aware of University policy and
- * regulations on honesty in academic work, and of the disciplinary
- * guidelines and procedures applicable to breaches of such
- * policy and regulations, as contained in the website.
- *
- * I have read and understood the University璽�砂�┬ policy on
- * the use of AI for academic work. I confirm that I have complied
- * with the instructions given by my course teacher(s) regarding the
- * use of AI tools for this project work and consent to the
- * use of AI content detection software to review my submission.
- *
-
- * University Guideline on Academic Honesty:
- *   https://www.cuhk.edu.hk/policy/academichonesty/
- *
- * Student Name  : Cheng Wing Yan
- * Student ID    : 1155257516
- * Class/Section : ENGG1110E
- * Date          :
- */
-
-
-/* Passing all test cases DOES NOT guarantee full marks.
-   Please follow the specifications and test more cases on your own */
-
-
 #include <stdio.h>
-
-/* NO other header files are allowed */
-
-/* NO global variables are allowed
-   Can declare your own local variable (inside functions) if needed */
 
 /* Macros (symbolic constants) */
 #define WIDTH 10
@@ -47,32 +7,15 @@
 #define COL 1
 
 
-/* DO NOT change the functions below, including the return type and parameters.
-   Any changes will result in mark deduction.
-
-   You can define your own functions if needed. */
-
 // Part one functions
 void printMaze(char mazeGameBoard[HEIGHT][WIDTH], int player[2]);
 int movePlayerOneStep(char mazeGameBoard[HEIGHT][WIDTH], int player[2], int input);
 void mirrorMaze(char mazeGameBoard[HEIGHT][WIDTH], int player[2]);
 
-
-// // Uncomment the later functions in part two
-// // crl+L or cmd+L to comment/uncomment
-// // Part two functions
-// void initGameBoardFromFile(char mazeGameBoard[HEIGHT][WIDTH], int player[2]);
-// void rotateMaze90(char mazeGameBoard[HEIGHT][WIDTH], int player[2]) ;
-// int movePlayerMultiSteps(char mazeGameBoard[HEIGHT][WIDTH], int player[2], int input) ;
-
-
-
 ////////////////////Part One///////////////////////////////
 
 
-/* Display the game board on the screen
-   You are required to follow exactly the output format stated in the project specification
- */
+/* Display the game board on the screen */
 void printMaze(char mazeGameBoard[HEIGHT][WIDTH], int player[2]){
     /* Output Message */
     printf("Maze:\n");
@@ -149,61 +92,6 @@ void mirrorMaze(char mazeGameBoard[HEIGHT][WIDTH], int player[2]){
     player[1]=WIDTH-player[1]-1;
     printf("You found a mirror item! Maze is now mirrored.\n");
 }
-
-
-
-
-
-////////////////////Part Two///////////////////////////////
-
-// // Uncomment the later functions in part two
-// // crl+L or cmd+L to comment/uncomment
-
-// /* Initialize the maze game board and player position from a file
-//    If failed, terminate the program immediantly
-//  */
-// void initGameBoardFromFile(char mazeGameBoard[HEIGHT][WIDTH], int player[2]){
-//     // ...
-//     // fill in the function
-
-//     /* Output Message */
-//     // printf("Error: Unable to open gameboard.txt\n");
-//     // printf("=====Game Over=====");
-// }
-
-
-
-// /* Rotate the maze 90 degrees clockwise
-//    Update the player's position (rotate as well)
-//  */
-// void rotateMaze90(char mazeGameBoard[HEIGHT][WIDTH], int player[2]){
-//     // ...
-//     // fill in the function
-
-//     /* Output Message */
-//     // printf("You found a rotation item! Maze is now rotated.\n");
-
-// }
-
-
-
-// /* Move the player continuously in the input direction until the condition met given by the project specification
-//    Return moved steps
-//  */
-// int movePlayerMultiSteps(char mazeGameBoard[HEIGHT][WIDTH], int player[2], int input){
-//     // ...
-//     // fill in the function
-
-//     /* Output Message */
-//     // printf("You hit a wall!\n");
-// }
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////
-
 
 /* The main function */
 
